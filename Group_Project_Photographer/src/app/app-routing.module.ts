8f4f17188fd00a7f252836144f95ac25fcb987b0
rikuddo91 <gullkhan0@live.com>
2022-10-18 13:16:12 +0200

    Finished Contact Page

--- Group_Project_Photographer/src/app/app-routing.module.ts
+++ Group_Project_Photographer/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactRegisterComponent } from './contact-register/contact-register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"contact-register", component: ContactRegisterComponent,
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
