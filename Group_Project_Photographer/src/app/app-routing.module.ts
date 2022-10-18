64abb05033ff0bb900da7039e467c75d1fda0a3d
Magdalena <kokanovic422@gmail.com>
2022-10-18 14:28:03 +0200

    ...

--- Group_Project_Photographer/src/app/app-routing.module.ts
+++ Group_Project_Photographer/src/app/app-routing.module.ts
@@ -1,35 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
-import { TestimonialsComponent } from './testimonials/testimonials.component';
-
-const routes: Routes = [
-
-
-  {
-    path: "testimonials", component: TestimonialsComponent
-
-
-  }
-];
-=======
 import { ContactRegisterComponent } from './contact-register/contact-register.component';
 import { BioAboutComponent } from './bio-about/bio-about.component';
 import { HomeComponent } from './home/home.component';
 import { PortofolioComponent } from './portofolio/portofolio.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+
 
 const routes: Routes = [
   {
-    path: '', component: HomeComponent,
-  },,{
-    path:"bio-about", component: BioAboutComponent,
-  } {
+    path: '', component: HomeComponent
+  }, {
+    path: "bio-about", component: BioAboutComponent
+  }, {
     path: 'portofolio', component: PortofolioComponent
   }, {
-    path: "contact-register", component: ContactRegisterComponent,
+    path: "contact-register", component: ContactRegisterComponent
+  },
+  {
+    path: "testimonials", component: TestimonialsComponent
   }];
->>>>>>> 328bba2a29dc80d5a1765d50f8ef3985acc07922
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
