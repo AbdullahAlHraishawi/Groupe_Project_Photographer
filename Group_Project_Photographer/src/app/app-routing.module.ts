a8fee5f9b4735d0683b70004b2c573d93379e4e4
rikuddo91 <gullkhan0@live.com>
2022-10-18 13:56:32 +0200

    Added Routing For Bio and Home

--- Group_Project_Photographer/src/app/app-routing.module.ts
+++ Group_Project_Photographer/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactRegisterComponent } from './contact-register/contact-register.component';
+import { BioAboutComponent } from './bio-about/bio-about.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path:"contact-register", component: ContactRegisterComponent,
+  },{
+    path:"",component: HomeComponent,
+  },{
+    path:"bio-about", component: BioAboutComponent,
   }
+
   
 ];
 
