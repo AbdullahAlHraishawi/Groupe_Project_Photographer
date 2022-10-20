f77c33e10b65b9f57dcb2482186e919438f571d2
Magdalena <kokanovic422@gmail.com>
2022-10-20 09:43:05 +0200

    ..

--- Group_Project_Photographer/src/app/app-routing.module.ts
+++ Group_Project_Photographer/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { PortofolioDetailComponent } from './portofolio-detail/portofolio-detail
 import { TestimonialsComponent } from './testimonials/testimonials.component';
 import { AwardsComponent } from './awards/awards.component';
 import { TestSwiperComponent } from './test-component/test-component.component';
-
+import { ExperienceComponent } from './experience/experience.component';
 
 const routes: Routes = [
   {
@@ -24,6 +24,8 @@ const routes: Routes = [
     path: "testimonials", component: TestimonialsComponent
   }, {
     path: "awards", component: AwardsComponent
+  },{
+path: "experience", component: ExperienceComponent
   }];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
